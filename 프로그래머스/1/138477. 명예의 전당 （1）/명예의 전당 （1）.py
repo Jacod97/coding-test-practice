a3903223b842c5040a858ff5b759111e35b5160e
Jacod97 <wotlr6894@naver.com>
2024-12-16 13:58:50 +0900

    [level 1] Title: 명예의 전당 (1), Time: 41.14 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,14 @@
+import itertools
+
+def solution(k, score):
+    stack = []   
+    answer = []
+    for i in range(len(score)):
+        if i >= k:
+            new_score = sorted(score[0:i+1])
+            new_score.reverse()
+            stack.append(new_score[0:k])
+        else:
+            stack.append(score[0:i+1])
+        answer.append(min(stack[i]))
+    return answer
