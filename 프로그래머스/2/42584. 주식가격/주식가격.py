67323483123a5b527ab85281e38a29339f7e4bb3
Jacod97 <wotlr6894@naver.com>
2024-12-22 13:42:04 +0900

    [level 2] Title: 주식가격, Time: 77.02 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,18 @@
+from collections import deque
+
 def solution(prices):
-    answer = [] # 각 가격마다 초를 담아둘 공간
+    stack = []
+    q = deque(prices)
     
-    for i in range(len(prices)-1): # 마지막껀 무조건 0이라 계산안함
-        count = 0 
-        for j in range(i+1, len(prices)):
-            if prices[i] > prices[j]: # 이전 가격보다 작아지면 더 카운트 할 필요가 없어 멈춤
-                count += 1 # 근데 더 작아져도 도달시간까지는 측정
+    while len(q) > 1:
+        cp = q.popleft()
+        seconds = 0
+        
+        for np in q:
+            seconds += 1   
+            if cp > np:
                 break
-            else: # 이전가격보다 크면 1초씩 증가
-                count += 1
-        answer.append(count)
-    answer.append(0) # 마지막 가격은 비교할 가격이 없기때문에 무조건 0
-    return answer
+                
+        stack.append(seconds)
+    stack.append(0)
+    return stack
