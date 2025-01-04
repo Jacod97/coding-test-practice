8a59a8a4eaa0ee69a3bd0b26870a417ac9117e50
Jacod97 <wotlr6894@naver.com>
2025-01-05 00:28:08 +0900

    [level 1] Title: 옹알이 (2), Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,21 @@
+import re
+
+def mussg(babbling, accent):
+    result = []
+    for baby in babbling:
+        if accent+accent in baby:
+            continue
+        temp = baby.replace(accent,'0')
+        result.append(temp)
+    return result
+
+def solution(babbling):
+    joka = ["aya", "ye", "woo", "ma"]
+    
+    for umm in joka:
+        babbling = mussg(babbling, umm)
+    answer = []
+    for i in babbling:
+        a = i.replace('0', '')
+        answer.append(a)
+    return answer.count('')
