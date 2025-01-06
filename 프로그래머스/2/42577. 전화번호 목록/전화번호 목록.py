d637796df0e17010ceeb3a62400906ed17723ae4
Jacod97 <wotlr6894@naver.com>
2025-01-06 18:45:43 +0900

    [level 2] Title: 전화번호 목록, Time: 150.89 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,15 @@
 def solution(phone_book):
-    # 앞자리 숫자를 기준으로 정렬(오름차순 아님)
+    phone = {}
+    # 사전순으로 정렬 후 폰번호와 길이를 딕셔너리에 저장
+    # (해시 문제라서 굳이 딕셔너리에 넣었지만 "굳이"인것 같다..)
     phone_book.sort()
-
-    # 반복문을 수행하면서 현재 번호와 다음 번호만 접두사 비교를 해줌
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+    for number in phone_book:
+        phone[number] = len(number)
+    # 머가리 길이 추출
+    head = list(phone.values())
+    # 머가리 길이만큼 문자열 슬라이싱 해주는데 같으면 false
+    for i in range(len(head)-1):
+        if phone_book[i] == phone_book[i+1][:head[i]]:
             return False
-
+            break
     return True
