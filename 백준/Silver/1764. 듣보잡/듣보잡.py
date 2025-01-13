1cc69939351f304470396f64312c5babcfefa7c2
Jacod97 <wotlr6894@naver.com>
2025-01-13 13:24:06 +0900

    [Silver IV] Title: 듣보잡, Time: 2580 ms, Memory: 44192 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,19 +1,13 @@
-# 첫째 줄: 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M
-N, M = map(int, input().split())
+n,m = map(int, input().split()) # 앞의 글자는 n, 뒤의 글자는 m으로 할당됨.
+n_set = set()
+m_set = set()
+for _ in range(n):
+    n_set.add(input())
+for _ in range(m):
+    m_set.add(input())
 
-# 듣도 못한 사람의 명단을 set에 저장
-not_heard = set()
-for _ in range(N):
-    not_heard.add(input().strip())
+def backjoon(x,y):
+    return sorted(list(x&y))
 
-# 보도 못한 사람의 명단을 set에 저장
-not_seen = set()
-for _ in range(M):
-    not_seen.add(input().strip())
-
-# 두 집합의 교집합 구하기
-result = sorted(not_heard & not_seen)
-
-# 결과 출력
-print(len(result))
-print('\n'.join(result))
+print(len(backjoon(n_set,m_set)))
+print('\n'.join(backjoon(n_set,m_set)))
