0b18a4e7ccad55e5894e17756002be624968e4a1
Jacod97 <wotlr6894@naver.com>
2025-02-08 18:09:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.52 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,23 +1,27 @@
-import re
+def binary(val,n):
+    result = []
+    weights = [2**i for i in sorted(range(n), reverse=True)]
+    for x in weights:
+        if val >= x:
+            result.append("1")
+            val -= x
+        else:
+            result.append("0")
+    return result
 
-def create_map(n,arr):
-    two_map = [bin(i) for i in arr]
-    complet_map = []
+def solution(n,arr1,arr2):
+    arr = []
     for i in range(n):
-        complet_map.append([int(x) for x in two_map[i] if x != 'b'])
-        if len(complet_map[i]) > n:
-            del complet_map[i][0]
-        elif len(complet_map[i]) < n:
-            complet_map[i] = [0] * (n-len(complet_map[i])) + complet_map[i]
-    return complet_map
+        memory = []
+        first_map = binary(arr1[i],n)
+        second_map = binary(arr2[i],n)
+        for j in range(n):
+            if int(first_map[j]) + int(second_map[j]) > 0:
+                memory.append("#")
+            else:
+                memory.append(" ")
+        arr.append(''.join(memory))
 
-def solution(n, arr1, arr2): 
-    arr1 = create_map(n,arr1)
-    arr2 = create_map(n,arr2)
+    return arr
+
     
-    answer = [[str(c+d) for c,d in zip(a,b)] for a,b in zip(arr1,arr2)]
-    for i in range(n):
-        answer[i] = ''.join(answer[i])
-        answer[i] = re.sub('[1-2]', '#', answer[i])
-        answer[i] = re.sub('[0]', ' ', answer[i])
-    return answer
