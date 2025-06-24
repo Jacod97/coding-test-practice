8c2c9abbeefbf8f3e3d9a089095fb23b80d81e93
Jacod97 <wotlr6894@naver.com>
2025-06-24 22:24:08 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,35 +1,39 @@
+def convert_time(time):
+    if type(time) == str:
+        conv_time = int(time[:2])*60 + int(time[3:])
+    else:
+        if time // 60 < 10:
+            head = f"0{time//60}"
+        else:
+            head = str(time//60)
+        if time % 60 < 10:
+            tail = f"0{time%60}"
+        else:
+            tail = str(time%60)
+        conv_time = f"{head}:{tail}"
+    return conv_time
+
 def solution(video_len, pos, op_start, op_end, commands):
-    function = [video_len, pos, op_start, op_end]
-    second = [(int(i[:2]) * 60) + int(i[-2:]) for i in function]
+    i_pos = convert_time(pos)
+    i_start = convert_time(op_start)
+    i_end = convert_time(op_end)
+    i_len = convert_time(video_len)
+    if i_pos >= i_start and i_pos <= i_end:
+        i_pos = i_end
+    for c in commands:
+        if c == "next":
+            i_pos += 10
+            if i_pos > i_len:
+                i_pos = i_len
+        elif c == "prev":
+            i_pos -= 10
+            if i_pos < 0:
+                i_pos = 0
+        if i_pos >= i_start and i_pos <= i_end:
+            i_pos = i_end
+
+    return convert_time(i_pos)
     
-    # commands 작동 전 pos
-    if second[2] <= second[1] < second[3]:
-        second[1] = second[3]
-    # commands 작동
-    for i in range(len(commands)):
-        if commands[i] == 'next':
-            second[1] += 10
-        elif commands[i] == 'prev':
-            second[1] -= 10
-    # pos 경계    
-        if second[1] < 0:
-            second[1] = 0
-        if second[1] > second[0]:
-            second[1] = second[0]
-        if second[2] <= second[1] < second[3]:
-            second[1] = second[3]
-            
-    # second -> "분:초"로 바꾸기
-    m = f"{second[1] // 60}"
-    if 0 < second[1] // 60 < 10:
-        m = "0"+m
-    elif second[1] // 60 == 0:
-        m = "00"
-    
-    s = f"{second[1] % 60}"
-    if 0 < second[1] % 60 < 10:
-        s = "0"+s
-    elif second[1] % 60 == 0:
-        s = "00"
-        
-    return m+':'+s
+# 10초 전 이동
+# 10초 후 이동
+# 오프닝 건너뛰기
